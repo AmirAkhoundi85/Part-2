b823ceb2b1724a97c2cc1b01f98a9b544b618a32
Amir <amir.akhoundi85@gmail.com>
2023-12-27 13:11:24 +0200

    compeleted part 2-2

--- src/components/Course.jsx
+++ src/components/Course.jsx
@@ -3,10 +3,14 @@ import Header from "./Header";
 import Content from "./Content";
 
 const Course = ({ course }) => {
+  let total = 0;
+  course.parts.forEach((item) => (total += item.exercises));
+
   return (
     <div>
       <Header name={course.name} />
       <Content parts={course.parts} />
+      <h4>total of {total} exercises</h4>
     </div>
   );
 };
