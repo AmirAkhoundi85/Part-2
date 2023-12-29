8d585712ced851991f9a5f8c17441fc56c170754
Amir <amir.akhoundi85@gmail.com>
2023-12-29 12:33:46 +0200

    completed 2-6

--- src/App.js
+++ src/App.js
@@ -1,13 +1,37 @@
+import { useState } from "react";
 import "./App.css";
 import CoursesList from "./components/CoursesList";
 
 function App() {
-  return (
-    <>
-      <h1>Web development curriculumn</h1>
-      <CoursesList />
-    </>
-  );
-}
+   const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+   const [newName, setNewName] = useState("");
+
+   const getName = (e)=>{
+    setNewName(e.target.value)
+   }
+
+   const addName = (e)=>{
+    e.preventDefault()
+    const newPersons = {name:newName}
+    setPersons(persons.concat(newPersons))
+    setNewName("")
+   }
+
+   return (
+     <div>
+       <h2>Phonebook</h2>
+       <form onSubmit={addName}>
+         <div>
+           name: <input value={newName} onChange={getName} />
+         </div>
+         <div>
+           <button type="submit">add</button>
+         </div>
+       </form>
+       <h2>Numbers</h2>
+       {persons.map((item)=><p>{item.name}</p>)}
+     </div>
+   );
+   }
 
 export default App;
