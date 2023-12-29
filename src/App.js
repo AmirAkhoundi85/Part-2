e04c84ec34dcaca6b82c3bc646b19a5768d63248
Amir <amir.akhoundi85@gmail.com>
2023-12-29 13:40:21 +0200

    completed 2-8

--- src/App.js
+++ src/App.js
@@ -3,26 +3,38 @@ import "./App.css";
 import CoursesList from "./components/CoursesList";
 
 function App() {
-   const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+   const [persons, setPersons] = useState([{ name: "Arto Hellas", phone: "" }]);
    const [newName, setNewName] = useState("");
+   const [phone, setPhone] = useState("");
 
-   const getName = (e)=>{
-    setNewName(e.target.value)
-   }
+   const getName = (e) => {
+     setNewName(e.target.value);
+   };
 
-   const addName = (e)=>{
-    e.preventDefault()
-    const person=  persons.find((item)=> item.name == newName)
-    
-    if(!person){
-    const newPersons = {name:newName}
-    setPersons(persons.concat(newPersons))
-    }else{
-      alert(`${newName} is already added to phonebook`);
-    }
+   const getNumber = (e) => {
+     setPhone(e.target.value);
+   };
 
-    setNewName("")
-   }
+   const addName = (e) => {
+     e.preventDefault();
+
+     const person = persons.find(
+       (item) => item.name === newName || item.phone === phone
+     );
+
+     if (!person) {
+       const newPerson = { name: newName, phone: phone };
+       setPersons([...persons, newPerson]);
+     } else {
+       alert(
+         `${newName} with phone number ${phone} is already added to the phonebook`
+       );
+     }
+
+     setNewName("");
+     setPhone("");
+   };
+   
 
    return (
      <div>
@@ -31,6 +43,7 @@ function App() {
          <div>
            name: <input value={newName} onChange={getName} />
          </div>
+         <div>number : <input value={phone} onChange={getNumber}  /></div>
          <div>
            <button type="submit">add</button>
          </div>
