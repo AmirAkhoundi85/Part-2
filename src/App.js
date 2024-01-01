1e368b50ed8de928e99b1a55a1b49c2ed0aa2103
Amir <amir.akhoundi85@gmail.com>
2024-01-01 11:51:38 +0200

    completed 2-9

--- src/App.js
+++ src/App.js
@@ -3,9 +3,15 @@ import "./App.css";
 import CoursesList from "./components/CoursesList";
 
 function App() {
-   const [persons, setPersons] = useState([{ name: "Arto Hellas", phone: "" }]);
+  const [persons, setPersons] = useState([
+    { name: "Arto Hellas", number: "040-123456", id: 1 },
+    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
+    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
+    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
+  ]);
    const [newName, setNewName] = useState("");
    const [phone, setPhone] = useState("");
+   const [searchTerm, setSearchTerm] = useState("");
 
    const getName = (e) => {
      setNewName(e.target.value);
@@ -34,6 +40,13 @@ function App() {
      setNewName("");
      setPhone("");
    };
+   const handelSearch = (e)=>{
+    setSearchTerm(e.target.value)
+   }
+
+   const filteredPersons = persons.filter((person) =>
+     person.name.toLowerCase().includes(searchTerm.toLowerCase())
+   );
    
 
    return (
@@ -41,15 +54,26 @@ function App() {
        <h2>Phonebook</h2>
        <form onSubmit={addName}>
          <div>
+           Filter shown with{" "}
+           <input value={searchTerm} onChange={handelSearch} />
+         </div>
+         <div>
+           <h3>add a new</h3>
            name: <input value={newName} onChange={getName} />
          </div>
-         <div>number : <input value={phone} onChange={getNumber}  /></div>
+         <div>
+           number : <input value={phone} onChange={getNumber} />
+         </div>
          <div>
            <button type="submit">add</button>
          </div>
        </form>
        <h2>Numbers</h2>
-       {persons.map((item)=><p>{item.name}</p>)}
+       {filteredPersons.map((item, index) => (
+         <p key={index}>
+           {item.name} {item.number}
+         </p>
+       ))}
      </div>
    );
    }
