a8c4ee22820f36c1f6cf37a2d28eaff1721f870d
Amir <amir.akhoundi85@gmail.com>
2024-01-03 13:51:59 +0200

    completed 2-15

--- src/App.js
+++ src/App.js
@@ -5,25 +5,24 @@ import Filter from "./components/Filter";
 import PersonForm from "./components/PersonForm";
 import Persons from "./components/Persons";
 import axios from "axios";
-import personService from "./servises/persons"
+import personService from "./servises/persons";
 
 function App() {
   const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState("");
   const [number, setPhone] = useState("");
   const [searchTerm, setSearchTerm] = useState("");
- 
 
-   useEffect(() => {
-      personService.getAll()
-        .then((data) => {
-          setPersons(data);
-        })
-        .catch((error) => {
-          console.log(error);
-        });
-   },[]);
-
+  useEffect(() => {
+    personService
+      .getAll()
+      .then((data) => {
+        setPersons(data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
 
   const getName = (e) => {
     setNewName(e.target.value);
@@ -36,23 +35,25 @@ function App() {
   const addName = (e) => {
     e.preventDefault();
 
-    const person = persons.find(
-      (item) => item.name === newName || item.number === number
-    );
+    const person = persons.find((item) => item.name === newName);
+    const newPerson = { name: newName, number: number };
 
     if (!person) {
-      const newPerson = { name: newName, number: number };
-      
-      personService.create(newPerson)
-      .then((data)=>{
+      personService.create(newPerson).then((data) => {
         setPersons([...persons, data]);
       });
-
-
     } else {
-      alert(
-        `${newName} with number number ${number} is already added to the phonebook`
+      const result = window.confirm(
+        `${newName} is already added to the phonebook, replace the old number with a new one?`
       );
+      if (result) {
+        personService.update(person.id, newPerson).then((data) => {
+          const newPersons = persons.map((item) =>
+            item.id != data.id ? item : data
+          );
+          setPersons(newPersons);
+        });
+      }
     }
 
     setNewName("");
@@ -63,21 +64,20 @@ function App() {
     setSearchTerm(e.target.value);
   };
 
-
   const filteredPersons = persons.filter((person) =>
     person.name.toLowerCase().includes(searchTerm.toLowerCase())
   );
 
-  const removePerson = (id, name)=>{
+  const removePerson = (id, name) => {
     // const person= persons.find((item)=>item.id== id)// item2
     const result = window.confirm(`Delete ${name} ?`);
     if (result) {
-       personService.remove(id).then((data) => {
-         const newPersons= persons.filter((item)=>item.id !==id)//[ item1, item3, item4]
-        setPersons(newPersons); 
-       });
+      personService.remove(id).then((data) => {
+        const newPersons = persons.filter((item) => item.id !== id); //[ item1, item3, item4]
+        setPersons(newPersons);
+      });
     }
-  }
+  };
 
   return (
     <div>
@@ -93,7 +93,6 @@ function App() {
       />
       <h3>Numbers</h3>
       <Persons filteredPersons={filteredPersons} removePerson={removePerson} />
-      
     </div>
   );
 }
