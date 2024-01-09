38392537b7b14fc075c198d5fae6337cbec1bbd5
Amir <amir.akhoundi85@gmail.com>
2024-01-09 13:18:24 +0200

    cmpleted2-19

--- src/components/Contries.jsx
+++ src/components/Contries.jsx
@@ -1,16 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Country from './Country';
 
 const Countries = ({filterCountries}) => {
+    const [countryInfo, setCountryInfo] = useState("")
+   
   return (
     <div>
       {filterCountries.length === 1 ? (
-        <Country countryInfo={filterCountries[0]}/>
+        <Country countryInfo={filterCountries[0]} />
       ) : filterCountries.length > 10 ? (
         <p>Too many matches, specify another filter</p>
       ) : (
         filterCountries.map((item) => (
-          <div key={item.name.common}>{item.name.common}</div>
+          <div key={item.name.common}>
+            <span>{item.name.common}</span>
+            <button onClick={() => setCountryInfo(item)}>show</button>
+            {countryInfo === item && <Country countryInfo={countryInfo} />}
+          </div>
         ))
       )}
     </div>
