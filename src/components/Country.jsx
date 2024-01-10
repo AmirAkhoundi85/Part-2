ff18aedc4cc416ecdcee3d05389fb18d49ae051c
Amir <amir.akhoundi85@gmail.com>
2024-01-10 13:17:49 +0200

    completed part2-20

--- src/components/Country.jsx
+++ src/components/Country.jsx
@@ -1,10 +1,20 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 
 const Country = ({ countryInfo }) => {
   const languages = Object.values(countryInfo.languages);
+  const api_key= process.env.REACT_APP_API_KEY_2;
+  const[weather, setWeather]= useState(null)
+
+  useEffect(() => {
+    axios.get(
+      `https://api.openweathermap.org/data/2.5/weather?q=${countryInfo.capital}&appid=${api_key}`
+    ).then((res)=>res.data)
+    .then((data)=>setWeather(data));
+
+  }, [])
   
-
-
+  
   return (
     <>
       <h1>{countryInfo.name.common}</h1>
@@ -19,6 +29,14 @@ const Country = ({ countryInfo }) => {
         ))}
       </ul>
       <img src={countryInfo.flags.png} alt={countryInfo.flags.alt} />
+      <h2>Weather in {countryInfo.name.common}</h2>
+      {weather && (
+        <div>
+          <p>temprature {(weather.main.temp - 273).toFixed(2)}</p>
+          <img src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`} alt="" />
+          <p>Wind {weather.wind.speed} m/s</p>
+        </div>
+      )}
     </>
   );
 };
